06308c806e9091ddff029af95b197e4a457bae3f
Min Cho <minsangcho19@gmail.com>
2022-02-23 14:48:42 -0800

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+
+    # def clickbait
+    #     unless email.match?(/flatironschool.com/)
+    #       errors.add(:email, "We're only allowed to have people who work for the company in the database!")
+    #     end
+    # end
 end
